4161b98997f385e9fd2b0b3a30506850cce4c0af
chenjiajia <chenjiajia530@163.com>
2018-01-10 19:19:51 +0800

    。。。

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,11 +13,20 @@
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
         <!-- v-if 防止created未调用到数据 seller.supports[0] 为undefined-->
+
         <div v-if="seller.supports" class="support">
-          <span class="icon"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div v-if="seller.supports" class="support-count">
+        <span class="count" >{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
+    </div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
     </div>
   </div>
 </template>
@@ -28,7 +37,10 @@
       seller: {
         type: Object
       }
-    }
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+  }
   };
 </script>
 
@@ -36,20 +48,92 @@
   @import "../../common/stylus/mixin.styl";
   .header
     color: #fff
-    background: #000
+    background: #999
     .content-wrapper
       display: flex
       padding: 24px 12px 18px 24px
       font-size: 14px
+      position: relative
+      .avatar
+        img
+          border-radius: 4px
       .content
         margin-left: 16px
         .title
-          margin: 2px 0 8px 0
+          margin: 0px 0 8px 0
           .brand
             display: inline-block
             width: 30px
             height: 18px
+            vertical-align: top;
             bg-image('brand')
             background-size: 30px 18px
-
+            background-repeat: no-repeat
+          .name
+            margin-left: 6px
+            font-size: 16px
+            font-weight: bold
+            line-height: 16px
+        .description
+          margin-bottom: 10px
+          line-height: 12px
+        .support
+          .icon
+            display: inline-block
+            width: 12px
+            height: 12px
+            margin-right: 4px
+            background-size: 12px 12px
+            background-repeat: no-repeat
+            vertical-align: bottom
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+          .text
+            font-size: 10px
+      .support-count
+        position: absolute
+        right: 12px
+        bottom: 18px
+        padding: 0 8px
+        height: 24px
+        font-size: 10px
+        line-height: 24px
+        border-radius: 14px
+        background-color: rgba(0, 0, 0, 0.2)
+        text-align: center
+    .bulletin-wrapper
+      position: relative
+      height: 28px
+      line-height: 28px
+      padding: 0 12px
+      white-space: nowrap
+      overflow: hidden
+      text-overflow: ellipsis
+      background: rgba(7, 17, 27, 0.2)
+      .bulletin-title
+        display: inline-block
+        vertical-align: top
+        margin-top: 7px
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text
+        vertical-align: top
+        margin: 0 4px
+        font-size: 10px
+      .icon-keyboard_arrow_right
+        position: absolute
+        font-size: 10px
+        right: 12px
+        top: 8px
 </style>
